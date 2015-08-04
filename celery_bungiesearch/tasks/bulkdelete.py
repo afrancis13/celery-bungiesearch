f4807197cb48da72a88a0b12c950902614f4b9f6
Alex Francis <afrancis@betterworks.com>
2015-08-04 15:28:11 -0700

    Add error handling code to bulk delete

--- celery_bungiesearch/tasks/bulkdelete.py
+++ celery_bungiesearch/tasks/bulkdelete.py
@@ -2,10 +2,18 @@ from .celerybungie import CeleryBungieTask
 from bungiesearch import Bungiesearch
 from bungiesearch.utils import update_index
 
+from elasticsearch import TransportError
+
 
 class BulkDeleteTask(CeleryBungieTask):
 
     def run(self, model, instances, **kwargs):
         settings = Bungiesearch.BUNGIE.get('SIGNALS', {})
         buffer_size = settings.get('BUFFER_SIZE', 100)
-        update_index(instances, model.__name__, action='delete', bulk_size=buffer_size)
+
+        try:
+            update_index(instances, model.__name__, action='delete', bulk_size=buffer_size)
+        except TransportError as e:
+            if e.status_code == 404:
+                return
+            raise
